b575de1246d3f250971b9a48d3e9872bda7a5fa6
maj-jinnah <maj.jinnah@gmail.com>
2022-11-25 15:19:09 +0600

    database connect done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -10,6 +10,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ea93pzh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const phonesCollection = client.db('alphaMobile').collection('phones')
+        const usersCollection = client.db('alphaMobile').collection('users')
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log)
 
 
 
