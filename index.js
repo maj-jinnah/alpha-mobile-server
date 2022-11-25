0a45284d92fdc69002cdf75222c650ba306b1bfa
maj-jinnah <maj.jinnah@gmail.com>
2022-11-25 14:59:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+// const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Now server is running!");
+});
+
+app.listen(port, () => {
+    console.log(`server is running, ${port}`);
+});
